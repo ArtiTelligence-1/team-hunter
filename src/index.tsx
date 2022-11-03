ffd7f3f1eff07e8da24c2633c8977bfb77110409
0verwritten <simplepcwriter@gmail.com>
2022-11-03 12:50:35 +0200

    changed webpack bundle

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 import { Provider } from 'react-redux';
 import SiteRouter from './routing/site-route';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 import store from './core/store';
 
@@ -22,4 +22,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
