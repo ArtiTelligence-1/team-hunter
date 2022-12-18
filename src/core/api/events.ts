d4158df77ac447ab760e6f957af27042142ca001
0verwritten <simplepcwriter@gmail.com>
2022-12-18 10:28:21 +0200

    made base query in separate file

--- src/core/api/events.ts
+++ src/core/api/events.ts
@@ -1,16 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { baseAPIQuery } from './baseData';
 
 export const eventApi = createApi({
   reducerPath: 'eventApi',
-  baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://team-hunter-back-staging.herokuapp.com/',
-    baseUrl: 'http://localhost:5086/api/v1/',
-    prepareHeaders: (headers, { getState }) => {
-      headers.set('Access-Control-Allow-Origin', '*');
-      headers.set('Content-Type', 'application/json');
-      return headers;
-    },
-  }),
+  baseQuery: baseAPIQuery,
   endpoints: (builder) => ({
     getEvent: builder.query<Event[], null>({
       query: () => ({
